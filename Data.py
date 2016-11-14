9af9da31867c7bb779bc2f62b1d2e6e2de3d5d89
cypmaster14 <lila.lazar@info.uaic.ro>
2016-11-14 23:02:27 +0200

    Add data for car example

--- Data.py
+++ Data.py
@@ -6,21 +6,21 @@ class Data:
     def __init__(self):
         self.atribute = None
 
-    def load_from_file(self, file_name, nr_params):
+    def load_from_file(self, nume_fisier, nr_parametri):
         """Incarca datele de antrenament din fisier in care fiecare linie
         reprezinta o instanta si are valorile fiecarui paramtru
         separate prin virgule
         """
-        self.n = nr_params + 1
-        self.data = [[] for _ in range(self.n)]  # empty list for each param + 1 one for label        
-        f = open(file_name, 'r')
-        lines = f.readlines()
-        f.close()
-        self.nr = len(lines)
-        for line in lines:
-            line = line[:-1]
-            valori_instanta = line.split(',')
-            for i in range(self.n):
+        self.nr_parametri = nr_parametri + 1
+        self.data = [[] for _ in range(self.nr_parametri)]  # empty list for each param + 1 one for label
+        fileObject = open(nume_fisier, 'r')
+        linii = fileObject.readlines()
+        fileObject.close()
+        self.nr_instante = len(linii)  # numarul de instante
+        for linie in linii:
+            linie = linie[:-1]
+            valori_instanta = linie.split(',')
+            for i in range(self.nr_parametri):
                 self.data[i].append(valori_instanta[i])
         self.compute_basic()
 
@@ -34,66 +34,68 @@ class Data:
         return entropie
 
     def compute_basic(self):
-        """Count the evaluation result and the data's overall entropy
         """
-        self.counter = Counter(self.data[-1]) #[9+,5-]
-        self.data_entropy = self.entropy(self.counter.values(), self.nr)
+            Clasifica datele si calculeaza entropia pasului in care ne aflam
+        """
+        self.counter = Counter(self.data[-1])  # [9+,5-]
+        self.data_entropy = self.entropy(self.counter.values(), self.nr_instante)
 
     def get_IG(self, atribut):
         """Determina IG pentru atributul o"""
 
-        counts = Counter(self.data[atribut])  # pentru fiecare atribut determin cate instante are fiecare valoare a atributului
+        counts = Counter(
+            self.data[atribut])  # pentru fiecare atribut determin cate instante are fiecare valoare a atributului
         information_gain = self.data_entropy
         for k, v in counts.items():
-            count_result = Counter([self.data[-1][i] for i in range(self.nr) if self.data[atribut][i] == k])
+            count_result = Counter([self.data[-1][i] for i in range(self.nr_instante) if self.data[atribut][i] == k])
             atr_entropy = self.entropy(count_result.values(), v)
-            information_gain -= (float(v) / self.nr) * atr_entropy
+            information_gain -= (float(v) / self.nr_instante) * atr_entropy
 
         return information_gain
 
     def get_all_IG(self):
-        return [(i, self.get_IG(i)) for i in range(self.n - 1)]
+        return [(i, self.get_IG(i)) for i in range(self.nr_parametri - 1)]
 
     def get_best_param(self):
         return sorted(self.get_all_IG(), key=lambda e: e[1], reverse=True)[0][0]
 
     def is_final(self):
-        """Final state if all instance are the same
-        or there is no parameter left
-        [6,0] sau m-am folosit de toti parametri
+        """Verific daca am ajuns intr-o stare finala adica:
+            -toate instantele sunt la fel (EX:[6+,0-])
+            -sau nu a mai ramas nici un parametru , i-am folosit pe toti
         """
-        return self.nr in self.counter.values() or self.n == 1
+        return self.nr_instante in self.counter.values() or self.nr_parametri == 1
 
     def set_atribute(self, p):
         self.atribute = p
 
     # def debug(self):
     #     s = ""
-    #     for i in range(self.nr):
-    #         for j in range(self.n):
+    #     for i in range(self.nr_instante):
+    #         for j in range(self.nr_parametri):
     #             s += "\t" + self.data[j][i]
-    #     print s
+    #     print (s)
 
 
-def create_from_other(other, param, value):
-    """Load training data set from other data set while eliminating
-    the parameter param and keeping only the instances where param 
-    had the specificated value. 
+def create_from_other(other, parametru, valoare):
+    """
+        Creeaza subarborele eliminand parametrul pe care tocmai l-am ales ca si atribut
+     si pastrez numai acele instante unde atributul are valoarea specificata
     """
     data = Data()
-    data.data = [[] for _ in range(other.n)]
-    data.nr = 0
-    for i in range(other.nr):
-        if other.data[param][i] == value:
-            data.nr += 1
-            for j in range(other.n):
+    data.data = [[] for _ in range(other.nr_parametri)]
+    data.nr_instante = 0
+    for i in range(other.nr_instante):
+        if other.data[parametru][i] == valoare:
+            data.nr_instante += 1
+            for j in range(other.nr_parametri):
                 data.data[j].append(other.data[j][i])
-    del data.data[param]
-    data.n = other.n - 1
+    del data.data[parametru]
+    data.nr_parametri = other.nr_parametri - 1
     data.compute_basic()
 
     if other.atribute:  # debug only
         data.atribute = [p for p in other.atribute]
-        del data.atribute[param]
+        del data.atribute[parametru]
 
     return data
--- Main.py
+++ Main.py
@@ -3,9 +3,22 @@ from Tree import getID3Tree
 
 d = Data()
 
-d.load_from_file("tennis.data", 4)
-d.set_atribute(["outlook", "temperature", "humidity", "wind"])
+# Code for play tennis
+
+# d.load_from_file("tennis.data", 4)
+# d.set_atribute(["outlook", "temperature", "humidity", "wind"])
+# tree = getID3Tree(d)
+#
+# print (tree.printTree(0))
+# print (tree.decide(["sunny", "cool", "high", "strong"]))
+
+# Code for car example
+
+d.load_from_file("car.data", 6)
+d.set_atribute(["buying", "maint", "doors", "persons", "lug_boot", "safety"])
+
 tree = getID3Tree(d)
 
-print tree.printTree(0)
-print tree.decide(["sunny", "cool", "high", "strong"])
+print (tree.printTree(0))
+print (tree.decide(["low", "high", "2", "4", "med", "low"]))
+print (tree.decide(["low", "low", "2", "more", "small", "high"]))
